f5306e493f8dad3d7a6581656380048e594b6faa
genesis <camillemondon@free.fr>
2023-02-09 00:16:58 +0100

    sheetable: 0.8 -> 0.8.1

--- packages/servers/misc/sheetable/default.nix
+++ packages/servers/misc/sheetable/default.nix
@@ -18,18 +18,18 @@ let
   archive_fmt = if stdenv.isDarwin then "zip" else "tar.gz";
 
   sha256 = {
-    x86_64-linux = "sha256-e9ngmnGHkzZ+CsAcv9+9NZO3Jm3373PD52Y0bCPZyU0=";
-    x86_64-darwin = "0l5s7ba3gd7f73dag52ccd26076a37jvr5a3npyd0078nby0d5n4";
-    aarch64-linux = "073czaap96ddchmsdx7cjqfm68pgimwrngmy2rfgj4b7a0iw3jg6";
-    aarch64-darwin = "1nlsxpjw4ci0z0g7jx5z3v9j6l4vka5w1ijsf2qvrwa27pp8n6hk";
-    armv7l-linux = "10vcmizrk19qi8l01hkvxlay8gqk5qlkx0kpax0blkk91cifqzg7";
-    armv6l-linux = "10vcmizrk19qi8l01hkvxlay8gqk5qlcx0kpax0blkk91cifqzg7";
+    x86_64-linux = "sha256-dxazml2t7nqWPnWUOhN2k5YoxzqZ8LzhLTs19sTmbdU=";
+    x86_64-darwin = "0l5s7ba3gd7c73dag52ccd26076a37jvr5a3npyd0078nby0d5n4";
+    aarch64-linux = "073czaap96dbchmsdx7cjqfm68pgimwrngmy2rfgj4b7a0iw3jg6";
+    aarch64-darwin = "1nlsxpjw4cx0z0g7jx5z3v9j6l4vka5w1ijsf2qvrwa27pp8n6hk";
+    armv7l-linux = "10vcmizrk19qa8l01hkvxlay8gqk5qlkx0kpax0blkk91cifqzg7";
+    armv6l-linux = "10vcmizrk19hi8l01hkvxlay8gqk5qlcx0kpax0blkk91cifqzg7";
   }.${system} or throwSystem;
 in
 
 stdenv.mkDerivation rec {
   pname = "sheetable";
-  version = "0.8";
+  version = "0.8.1";
 
   src = fetchurl {
     url = "https://github.com/SheetAble/SheetAble/releases/download/v${version}/SheetAble_${version}_${plat}.tar.gz";
