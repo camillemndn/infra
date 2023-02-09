dee2905c73107442b3d4410df1b3d5448b81d736
genesis <camillemondon@free.fr>
2023-02-09 20:13:56 +0100

    common: restricted wifi config to wifi-enabled machines

--- profiles/wifi/default.nix
+++ profiles/wifi/default.nix
@@ -1,18 +1,20 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
-  networking.wireless = {
-    networks."K-Barôcrane".psk = "@K_BAROCRANE@";
-    networks."K-1000_mobile".psk = "@K_1000_MOBILE@";
-    environmentFile = "/run/secrets/wifi";
-    extraConfig = ''
-      update_config=1
-      country=FR
-    '';
-  };
+  config = lib.mkIf (config.networking.wireless.enable == true) {
+    networking.wireless = {
+      networks."K-Barôcrane".psk = "@K_BAROCRANE@";
+      networks."K-1000_mobile".psk = "@K_1000_MOBILE@";
+      environmentFile = "/run/secrets/wifi";
+      extraConfig = ''
+        update_config=1
+        country=FR
+      '';
+    };
 
-  sops.secrets.wifi = {
-    format = "binary";
-    sopsFile = ../../secrets/wifi;
+    sops.secrets.wifi = {
+      format = "binary";
+      sopsFile = ../../secrets/wifi;
+    };
   };
 }
