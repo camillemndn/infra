64c09b849ee368bd19f4da5f3124d99e1865c7bb
genesis <camillemondon@free.fr>
2023-03-21 19:56:07 +0100

    zotero: init at 6.0.22

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15,5 +15,6 @@ with pkgs;
   sheetable = callPackage ../servers/sheetable { };
   webtrees = callPackage ../servers/webtrees { };
   zapzap = callPackage ../applications/networking/instant-messengers/zapzap { };
+  zotero = unstable.callPackage ../applications/office/zotero { };
   zotero-web-library = callPackage ../servers/zotero-web-library { };
 }
