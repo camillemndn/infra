2e60c33ed160e3f592616025ff0655c00180eed6
genesis <camillemondon@free.fr>
2023-03-22 01:46:22 +0100

    common: updated flake.lock and bumped kernel to 6.2.7

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
 
     nix-init.url = "github:nix-community/nix-init/c433a3b73869a66c8267c84425b8e0f9990c59e9";
 
-    kernel.url = "nixpkgs/7f5639fa3b68054ca0b062866dc62b22c3f11505";
+    kernel.url = "nixpkgs/4c83fe7ec352c9b81cf8220bd1653a1bce970069";
   };
 
   outputs = inputs@{ self, nixpkgs, unstable, home-manager, utils, deploy-rs, sops-nix, neovim-nightly-overlay, nixos-wsl, phps, ... }:
