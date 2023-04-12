6f0158be05643dd3105633117424f6c9a65600f7
genesis <camillemondon@free.fr>
2023-04-13 00:32:32 +0200

    common: cleaned flake inputs

--- flake.nix
+++ flake.nix
@@ -3,19 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     unstable.url = "github:camillemndn/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "unstable";
-      inputs.utils.follows = "utils";
-    };
-
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland-contrib.url = "github:hyprwm/contrib";
-
-    utils.url = "github:numtide/flake-utils";
+    kernel.url = "nixpkgs/4c83fe7ec352c9b81cf8220bd1653a1bce970069";
 
     deploy-rs = {
       url = "github:serokell/deploy-rs";
@@ -23,15 +12,47 @@
       inputs.utils.follows = "utils";
     };
 
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "unstable";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
     };
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "unstable";
+    };
+
+    hyprland-contrib = {
+      url = "github:hyprwm/contrib";
+      inputs.nixpkgs.follows = "unstable";
+    };
+
+    lanzaboote.url = "github:nix-community/lanzaboote";
 
     neovim-nightly-overlay = {
       url = "github:nix-community/neovim-nightly-overlay";
       inputs.nixpkgs.follows = "unstable";
+    };
+
+    nix-software-center = {
+      url = "github:vlinkz/nix-software-center";
+      inputs.nixpkgs.follows = "unstable";
+      inputs.utils.follows = "utils";
+    };
+
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "utils";
+    };
+
+    phps = {
+      url = "github:fossar/nix-phps";
+      inputs.nixpkgs.follows = "unstable";
+      inputs.utils.follows = "utils";
     };
 
     simple-nixos-mailserver = {
@@ -41,36 +62,22 @@
       inputs.utils.follows = "utils";
     };
 
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL";
-      inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "unstable";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
+
+    spicetify-nix = {
+      url = "github:the-argus/spicetify-nix";
+      inputs.nixpkgs.follows = "unstable";
       inputs.flake-utils.follows = "utils";
     };
 
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
-    phps = {
-      url = "github:fossar/nix-phps";
-      inputs.nixpkgs.follows = "unstable";
-      inputs.utils.follows = "utils";
-    };
-
-    lanzaboote.url = "github:nix-community/lanzaboote";
-
-    nix-software-center = {
-      url = "github:vlinkz/nix-software-center";
-      inputs.nixpkgs.follows = "unstable";
-      inputs.utils.follows = "utils";
-    };
-
-    nix-init.url = "github:nix-community/nix-init/c433a3b73869a66c8267c84425b8e0f9990c59e9";
-
-    kernel.url = "nixpkgs/4c83fe7ec352c9b81cf8220bd1653a1bce970069";
-
-    spicetify-nix.url = "github:the-argus/spicetify-nix";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, nixpkgs, unstable, home-manager, hyprland, hyprland-contrib, utils, deploy-rs, sops-nix, neovim-nightly-overlay, nixos-wsl, phps, ... }:
+  outputs = inputs@{ nixpkgs, unstable, utils, ... }:
     with builtins;
     (utils.lib.eachDefaultSystem
       (system:
@@ -80,9 +87,12 @@
               unstable = unstable.legacyPackages.${system};
             });
         in
-        rec {
-          packages = nixpkgs.lib.attrsets.filterAttrs (name: value: (elem (toString system) value.meta.platforms)) (import ./pkgs/top-level { inherit pkgs; });
-        }) // {
+        {
+          packages = nixpkgs.lib.attrsets.filterAttrs
+            (name: value: (elem (toString system) value.meta.platforms))
+            (import ./pkgs/top-level { inherit pkgs; });
+        }) //
+    {
       deploy = import ./deploy.nix inputs;
 
       homeConfigurations = import ./home/configurations inputs;
@@ -92,6 +102,5 @@
       nixosModules = import ./modules;
 
       # checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
-    }
-    );
+    });
 }
