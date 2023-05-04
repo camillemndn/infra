7867eadb97db01e9851755cf530d9578dce2791d
genesis <camillemondon@free.fr>
2023-05-04 17:17:57 +0200

    pinkfloyd: swapped to nftables, added a bunch of apps

--- configurations/pinkfloyd/default.nix
+++ configurations/pinkfloyd/default.nix
@@ -28,43 +28,26 @@
 
   # Auto-login for phosh
   services.xserver.desktopManager.phosh = {
-    user = "julien";
+    user = "camille";
   };
+  services.openssh.enable = true;
 
+  programs.firefox.enable = true;
+  environment.systemPackages = with pkgs; [ vlc ];
   #
   # User configuration
   #
 
-  users.users."julien" = {
-    isNormalUser = true;
-    description = "Julien";
-    hashedPassword = "$6$8RP9D.PjdHaFGGz7$CLWZH8ccKy1I5D.k9he9BMx2Q/qr.KAYVrLhn6r7V.6M6HV7JPaetP3nFho3nSLbBbA1gpj0KUqHqLaresZzQ1";
+  users.users."camille" = {
+    hashedPassword = "$y$j9T$xLuhy428hvJuTbKuJcbim0$EchthidZtxYp7QjZNrQt3SqdZ0mP2htnDiJFLuLNey7";
     extraGroups = [
       "dialout"
       "feedbackd"
-      "networkmanager"
       "video"
-      "wheel"
     ];
   };
-  services.openssh.enable = true;
 
-  nix = {
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      trusted-users = [ "julien" ];
-
-      substituters = [
-        "https://cache.mondon.xyz?priority=45"
-        "https://cache2.mondon.xyz?priority=45"
-      ];
-
-      trusted-public-keys = [
-        "cache.mondon.xyz:6o1j93GkK5gj0PfYouSA4WPAEEnOuPGTebLCWc/jKfQ="
-        "cache2.mondon.xyz:8zCLL6cuq3rX66LpesMMQRticIrMsewHXzl8NmPUvfs="
-      ];
-    };
-  };
+  networking.nftables.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
