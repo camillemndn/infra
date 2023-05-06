154e2defc62b3d1f1dfdb7ccbbaa358a2606699a
genesis <camillemondon@free.fr>
2023-05-07 00:46:11 +0200

    zotoro: cleaned derivation for upstreaming

--- pkgs/applications/office/zotero/default.nix
+++ pkgs/applications/office/zotero/default.nix
@@ -6,6 +6,7 @@
 , firefox-esr-102-unwrapped
 , makeWrapper
 , makeDesktopItem
+, copyDesktopItems
 , python3
 , unzip
 , zip
@@ -30,7 +31,7 @@ let
     homepage = "https://github.com/zotero/zotero";
     license = licenses.agpl3Only;
     maintainers = with maintainers; [ camillemndn ];
-    platforms = platforms.linux;
+    platforms = [ "x86_64-linux" "aarch64-linux" ];
   };
 
   pdftools = let pdftools-version = "0.0.5"; in
@@ -42,52 +43,50 @@ let
 
   zotero-client =
     let
-      src = fetchFromGitHub {
-        owner = "zotero";
-        repo = "zotero";
-        inherit rev;
-        hash = "sha256-XGpk2CYgdaFCJJJc2XhW2fVD+dbACUkGClgwLoNMOoM=";
-        fetchSubmodules = true;
-      };
-
       npmFlags = [ "--legacy-peer-deps" ];
       NODE_OPTIONS = "--openssl-legacy-provider";
 
       single-file = buildNpmPackage {
         pname = "${pname}-single-file";
+        inherit version npmFlags NODE_OPTIONS meta;
+
         src = fetchFromGitHub {
           owner = "gildas-lormeau";
           repo = "SingleFile";
           rev = "999976a20afb51a18da4abb42f434eac99796e84";
           hash = "sha256-fHKVn9DTwshfBXf0nxCk0MLY1fEiQXD/SZPG1bICHo8=";
         };
-        inherit version npmFlags NODE_OPTIONS meta;
+
         npmDepsHash = "sha256-L4LuD7n8c42TPpbLWuJzeM27xcsXVdBnMTqNvRZMdz8=";
         dontNpmBuild = true;
       };
 
       xpcom-utilities = buildNpmPackage {
         pname = "${pname}-xpcom-utilities";
+        inherit version npmFlags NODE_OPTIONS meta;
+
         src = fetchFromGitHub {
           owner = "zotero";
           repo = "utilities";
           rev = "b93f16dba483891c0ab4627cbaa303de5c7fa0c0";
           hash = "sha256-Oz3h6aGorAm+Y5JZSclfz40YRj+uSPW2a5jgQWszLsk=";
         };
-        inherit version npmFlags NODE_OPTIONS meta;
+
         npmDepsHash = "sha256-tWDADhAeXG0HSvFnpdGOya3CjSb0i2aR3E1Y3r1J81o=";
         dontNpmBuild = true;
       };
 
       note-editor = buildNpmPackage {
         pname = "${pname}-note-editor";
+        inherit version npmFlags NODE_OPTIONS meta;
+
         src = fetchFromGitHub {
           owner = "zotero";
           repo = "note-editor";
           rev = "076f5b3d3609051b9cba3cd68c4bb22746187834";
           hash = "sha256-ZDmb3DQttftfS4w5+HlkTXRxhRYftBh1bm6MI+RBvII=";
         };
-        inherit version npmFlags NODE_OPTIONS meta;
+
         npmDepsHash = "sha256-yu2s4V2hB07eS0INVxQXU7YeWYmR3p4JPxKWuCK3Iys=";
 
         postInstall = ''
@@ -97,20 +96,26 @@ let
 
       translators = buildNpmPackage {
         pname = "${pname}-translators";
+        inherit version npmFlags NODE_OPTIONS meta;
+
         src = fetchFromGitHub {
           owner = "zotero";
           repo = "translators";
           rev = "3a9544d7b0b6fdcb6cdcbc8c08392f91d20d99b4";
           hash = "sha256-XX1iBjuaFpmtkKTHuFPTtZOcFZk+oF8C8d2DPnENjV4=";
         };
-        inherit version npmFlags NODE_OPTIONS meta;
+
         npmDepsHash = "sha256-lHSUID8Gv+Rw6Ed+3SbxzdjvxFoCGiyY6BXb0U2A1eo=";
 
         postPatch = ''
           rm package-lock.json
           cp ${./translator-lock.json} package-lock.json
-          sed -i '/eslint-plugin-zotero-translator/d' package.json
-          echo "chromedriver_skip_download=true" >> .npmrc  
+
+          # Remove unused dependency
+          sed -i package.json -e '/eslint-plugin-zotero-translator/d'
+
+          # Avoid downloading binary in sandbox
+          echo "chromedriver_skip_download=true" >> .npmrc
         '';
 
         dontNpmBuild = true;
@@ -118,22 +123,22 @@ let
 
       pdf-reader-pdfjs = buildNpmPackage {
         pname = "${pname}-pdf-reader-pdfjs";
+        inherit version npmFlags NODE_OPTIONS meta;
+
         src = fetchFromGitHub {
           owner = "zotero";
           repo = "pdf.js";
           rev = "336247a15be77f2e253599f810a8cda107171566";
           hash = "sha256-GBEBTyFMh+zi0KiHLNGfZi/XW9mhm3x0uCJkA7ml7mk=";
         };
-        inherit version npmFlags NODE_OPTIONS meta;
+
         npmDepsHash = "sha256-9e90iIKwWyBq68q/CKn+7laJwPFtJaZtblcWpIEDSXw=";
         makeCacheWritable = true;
 
         postPatch = ''
-          sed -i '/"name": "pdf.js"/a "version": "1.0.0",' package.json
-          sed -i '/"name": "pdf.js"/a "version": "1.0.0",' package-lock.json
+          # Add a version number
+          sed -i package*.json -e '/"name": "pdf.js"/a "version": "1.0.0",'
         '';
-
-        #dontNpmBuild = true;
 
         buildPhase = ''
           node_modules/.bin/gulp generic
@@ -146,25 +151,25 @@ let
 
       pdf-worker-pdfjs = buildNpmPackage {
         pname = "${pname}-pdf-worker-pdfjs";
+        inherit version npmFlags NODE_OPTIONS meta;
+
         src = fetchFromGitHub {
           owner = "zotero";
           repo = "pdf.js";
           rev = "e198a17afc6f56e0a9d48b07e42ec80645a7a0a8";
           hash = "sha256-FlKII11oPMPka+96Wo9ZjBuNp40i3OMuvlNz8X/r0Lw=";
         };
-        inherit version npmFlags NODE_OPTIONS meta;
+
         npmDepsHash = "sha256-9e90iIKwWyBq68q/CKn+7laJwPFtJaZtblcWpIEDSXw=";
         makeCacheWritable = true;
 
         postPatch = ''
-          sed -i '/"name": "pdf.js"/a "version": "1.0.0",' package.json
-          sed -i '/"name": "pdf.js"/a "version": "1.0.0",' package-lock.json
+          # Add a version number
+          sed -i package*.json -e '/"name": "pdf.js"/a "version": "1.0.0",'
         '';
 
-        #dontNpmBuild = true;
-
         buildPhase = ''
-          node_modules/.bin/gulp lib 
+          node_modules/.bin/gulp lib
         '';
 
         postInstall = ''
@@ -184,12 +189,14 @@ let
         npmDepsHash = "sha256-tDr2WLnpltWPrlF21M8G/We4zzAXBp4px5xceOVLbhQ=";
 
         postPatch = ''
-          sed -i 's/npx gulp/#npx gulp/g' scripts/build-pdfjs
-          sed -i 's/npm ci/#npm ci/g' scripts/build-pdfjs
+          # Avoid npm install since it is handled by buildNpmPackage
+          sed -i scripts/build-pdfjs \
+            -e 's/npx gulp/#npx gulp/g' \
+            -e 's/npm ci/#npm ci/g'
         '';
 
         buildPhase = ''
-          rm -rf pdf.js 
+          rm -rf pdf.js
           cp -Lr ${pdf-reader-pdfjs}/lib/node_modules/pdf.js pdf.js
         '';
 
@@ -211,12 +218,14 @@ let
         npmDepsHash = "sha256-rO/P7/22erxNeOpR8ph7taKyCeOEG9+U06oOfmPSa3w=";
 
         postPatch = ''
-          sed -i 's/npx gulp/#npx gulp/g' scripts/build-pdfjs
-          sed -i 's/npm ci/#npm ci/g' scripts/build-pdfjs
+          # Avoid npm install since it is handled by buildNpmPackage
+          sed -i scripts/build-pdfjs \
+            -e 's/npx gulp/#npx gulp/g' \
+            -e 's/npm ci/#npm ci/g'
         '';
 
         buildPhase = ''
-          rm -rf pdf.js 
+          rm -rf pdf.js
           cp -Lr ${pdf-worker-pdfjs}/lib/node_modules/pdf.js pdf.js
         '';
 
@@ -228,50 +237,63 @@ let
     in
     buildNpmPackage {
       pname = "${pname}-client";
-      inherit src version npmFlags NODE_OPTIONS meta;
+      inherit version npmFlags NODE_OPTIONS meta;
+
+      src = fetchFromGitHub {
+        owner = "zotero";
+        repo = "zotero";
+        inherit rev;
+        hash = "sha256-XGpk2CYgdaFCJJJc2XhW2fVD+dbACUkGClgwLoNMOoM=";
+        fetchSubmodules = true;
+      };
+
       npmDepsHash = "sha256-b9MCHtt4Ewpt/prEMKtzSbLv3xnP2lnhclu4xDh1QGQ=";
-      nativeBuildInputs = [ rsync ];
+
       patches = [ ./dark-tabs.patch ];
 
       postPatch = ''
-        rm -rf resource/SingleFile 
+        rm -rf resource/SingleFile
         cp -Lr ${single-file}/lib/node_modules/single-file resource/SingleFile
 
         rm -rf chrome/content/zotero/xpcom/utilities
-        cp -Lr ${xpcom-utilities}/lib/node_modules/@zotero/utilities chrome/content/zotero/xpcom/utilities 
+        cp -Lr ${xpcom-utilities}/lib/node_modules/@zotero/utilities chrome/content/zotero/xpcom/utilities
 
-        rm -rf pdf-reader 
+        rm -rf pdf-reader
         cp -r ${pdf-reader}/lib/node_modules/pdf-reader pdf-reader
 
         rm -rf pdf-worker
         cp -r ${pdf-worker}/lib/node_modules/pdf-worker pdf-worker
 
         chmod +w . -R
-        
+
         (
           cd pdf-reader
-          rm -rf pdf.js 
+          rm -rf pdf.js
           cp -Lr ${pdf-reader-pdfjs}/lib/node_modules/pdf.js pdf.js
         )
         (
           cd pdf-worker
-          rm -rf pdf.js 
+          rm -rf pdf.js
           cp -Lr ${pdf-worker-pdfjs}/lib/node_modules/pdf.js pdf.js
         )
 
-        rm -rf translators 
-        cp -Lr ${translators}/lib/node_modules/translators-check translators 
+        rm -rf translators
+        cp -Lr ${translators}/lib/node_modules/translators-check translators
 
         rm -rf note-editor
         cp -Lr ${note-editor}/lib/node_modules/zotero-note-editor note-editor
 
         chmod +w . -R
 
+        # Avoid npm install and using git
         find scripts -type f | xargs sed -i 's/npm ci/#npm ci/g'
         find scripts -type f | xargs sed -i 's/git/#git/g'
 
-        sed -i 's/npm run build/#npm run build/g' scripts/note-editor.js
+        # Avoid npm build since it is already built
+        sed -i scripts/note-editor.js -e 's/npm run build/#npm run build/g'
       '';
+
+      nativeBuildInputs = [ rsync ];
 
       installPhase = ''
         mkdir $out
@@ -282,6 +304,7 @@ let
   zotero-build = stdenv.mkDerivation {
     pname = "${pname}-build";
     inherit version meta;
+
     src = fetchFromGitHub {
       owner = "zotero";
       repo = "zotero-build";
@@ -291,8 +314,8 @@ let
     };
 
     postPatch = ''
-      sed -i -E "/-aL/a '--chmod=Du=rwx'," xpi/build_xpi
-      #sed -i 's/-aL/-L/' xpi/build_xpi
+      # Make the copied files writable during rsync
+      sed -i xpi/build_xpi -Ee "/-aL/a '--chmod=Du=rwx',"
     '';
 
     buildInputs = [ python3 rsync perl ];
@@ -307,6 +330,25 @@ let
     '';
   };
 
+  desktopItem = makeDesktopItem {
+    name = pname;
+    exec = "${pname} -url %U";
+    icon = "zotero";
+    comment = meta.description;
+    desktopName = "Zotero";
+    genericName = "Reference Management";
+    categories = [ "Office" "Database" ];
+    startupNotify = true;
+    startupWMClass = pname;
+    mimeTypes = [ "x-scheme-handler/zotero" "text/plain" ];
+    terminal = false;
+    actions = {
+      profile-manager-window = {
+        name = "Profile Manager";
+        exec = "${pname} -P";
+      };
+    };
+  };
 in
 
 stdenv.mkDerivation {
@@ -323,21 +365,25 @@ stdenv.mkDerivation {
   patches = [ ./fetchxul.patch ];
 
   postPatch = ''
-    patchShebangs .
+    patchShebangs fetch_xulrunner.sh build.sh scripts/dir_build
 
-    sed -i 's|LINUX_i686_RUNTIME_PATH=.*|LINUX_i686_RUNTIME_PATH="$DIR/xulrunner/firefox"|' config.sh
-    sed -i 's|LINUX_x86_64_RUNTIME_PATH=.*|LINUX_x86_64_RUNTIME_PATH="$DIR/xulrunner/firefox"|' config.sh
-    sed -i 's|ZOTERO_SOURCE_DIR=.*|ZOTERO_SOURCE_DIR="${zotero-client}"|' config.sh
-    sed -i 's|ZOTERO_BUILD_DIR=.*|ZOTERO_BUILD_DIR="${zotero-build}"|' config.sh
+    # Correct the paths in config variables
+    sed -i config.sh \
+      -e 's|LINUX_i686_RUNTIME_PATH=.*|LINUX_i686_RUNTIME_PATH="$DIR/xulrunner/firefox"|' \
+      -e 's|LINUX_x86_64_RUNTIME_PATH=.*|LINUX_x86_64_RUNTIME_PATH="$DIR/xulrunner/firefox"|' \
+      -e 's|ZOTERO_SOURCE_DIR=.*|ZOTERO_SOURCE_DIR="${zotero-client}"|' \
+      -e 's|ZOTERO_BUILD_DIR=.*|ZOTERO_BUILD_DIR="${zotero-build}"|'
 
-    sed -i -E 's|(.*hash=).*|\1${rev}|' scripts/dir_build
-    sed -i '/build_xpi/d' scripts/dir_build
+    # Use the hash from this revision and avoid building xpi
+    sed -i scripts/dir_build \
+      -Ee 's|(.*hash=).*|\1${rev}|' \
+      -e '/build_xpi/d'
 
-    sed -i -E 's|(rsync -a.*)|\1; chmod -R +w $BUILD_DIR|' build.sh 
-    #sed -i 's|MaxVersion=.*|MaxVersion=111.0|' assets/application.ini 
+    # Make the copied files writable after rsync
+    sed -i build.sh -Ee 's|(rsync -a.*)|\1; chmod -R +w $BUILD_DIR|'
   '';
 
-  nativeBuildInputs = [ makeWrapper python3 unzip zip perl rsync wrapGAppsHook ];
+  nativeBuildInputs = [ makeWrapper python3 unzip zip perl rsync wrapGAppsHook copyDesktopItems ];
   buildInputs = [ gsettings-desktop-schemas glib gtk3 gnome.adwaita-icon-theme dconf ];
 
   configurePhase = ''
@@ -346,7 +392,7 @@ stdenv.mkDerivation {
     chmod -R +w xulrunner
 
     cp -Lr ${pdftools} pdftools
-    chmod -R +w pdftools 
+    chmod -R +w pdftools
 
     ./fetch_xulrunner.sh -p l
   '';
@@ -356,41 +402,20 @@ stdenv.mkDerivation {
     scripts/dir_build -p l
   '';
 
-  installPhase =
-    let
-      desktopItem = makeDesktopItem {
-        name = pname;
-        exec = "${pname} -url %U";
-        icon = "zotero";
-        comment = meta.description;
-        desktopName = "Zotero";
-        genericName = "Reference Management";
-        categories = [ "Office" "Database" ];
-        startupNotify = true;
-        startupWMClass = pname;
-        mimeTypes = [ "x-scheme-handler/zotero" "text/plain" ];
-        terminal = false;
-        actions = {
-          profile-manager-window = {
-            name = "Profile Manager";
-            exec = "${pname} -P";
-          };
-        };
-      };
-    in
-    ''
-      mkdir -p $out/bin
-      mkdir -p $out/usr/lib
-      cp -Lr staging/Zotero_linux $out/usr/lib/zotero-bin-${version}
-      ln -s $out/usr/lib/zotero-bin-${version}/zotero $out/bin
+  installPhase = ''
+    mkdir -p $out/{bin,lib}
+    cp -Lr staging/Zotero_linux $out/lib/zotero
+    ln -s $out/lib/zotero/zotero $out/bin
 
-      mkdir -p $out/share/applications
-      cp ${desktopItem}/share/applications/* $out/share/applications/
-      for size in 16 32 48 256; do
-        install -Dm444 staging/Zotero_linux/chrome/icons/default/default$size.png \
-          $out/share/icons/hicolor/''${size}x''${size}/apps/zotero.png
-      done
-    '';
+    for size in 16 32 48 256; do
+      install -Dm444 staging/Zotero_linux/chrome/icons/default/default$size.png \
+        $out/share/icons/hicolor/''${size}x''${size}/apps/zotero.png
+    done
+
+    runHook postInstall
+  '';
+
+  desktopItems = [ desktopItem ];
 
   preFixup = ''
     gappsWrapperArgs+=(
