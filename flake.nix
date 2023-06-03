5ba72cff7cb7cb3f9cf8bd27a7a024edf6bfaca3
genesis <camillemondon@free.fr>
2023-06-03 20:38:07 +0200

    common: create whole zones for dnsRecords

--- flake.nix
+++ flake.nix
@@ -117,19 +117,35 @@
             in
             [ (concatStringsSep sep (drop (length l - len) l)) (concatStringsSep sep (take (length l - len) l)) ];
 
-          isVPN = x: hasSuffix "kms" x || hasSuffix "saumon" x;
+          isVPN = x: hasSuffix "luj" x || hasSuffix "kms" x || hasSuffix "saumon" x;
 
           extractDomain = x:
             if (isVPN x) then (splitSuffix 1 "." x) else
             splitSuffix 2 "." x;
 
           domainToRecord = machine: x:
-            if !(hasInfix "." x) then { } else
+            if (!(hasInfix "." x) || hasInfix ".local" x) then { } else
             let
-              domain = head (extractDomain x);
+              zone = head (extractDomain x);
               subdomain = last (extractDomain x);
             in
-            { ${domain}.${subdomain}.A = with machineInfo.${machine}; if isVPN x then [ vpn ] else [ public ]; };
+            {
+              ${zone} = {
+                TTL = 60 * 60;
+                NS = [ "@" ];
+                SOA = {
+                  nameServer = "@";
+                  adminEmail = "dns@saumon.network";
+                  serial = 0;
+                };
+              } //
+              (if (subdomain == "") then {
+                A = with machineInfo.${machine};
+                  (if isVPN x then [ vpn ] else [ public ]);
+              } else {
+                subdomains.${subdomain}.A = with machineInfo.${machine}; if isVPN x then [ vpn ] else [ public ];
+              });
+            };
 
           getDomains = machine: with self.nixosConfigurations.${machine}.config; attrNames services.nginx.virtualHosts ++ optional services.tailscale.enable "${machine}.kms";
 
