1388d0049d69c8738300c4b8e0c83bdef0581fa3
genesis <camillemondon@free.fr>
2023-06-10 14:20:01 +0200

    chore: online url input system

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     };
 
     utils = { url = "flake-utils"; inputs.systems.follows = "systems"; };
-    systems = { url = "path:./systems.nix"; flake = false; };
+    systems = { url = "https://raw.githubusercontent.com/camillemndn/nixos-config/new/systems.nix"; flake = false; };
 
     # Hardware dependencies
 
