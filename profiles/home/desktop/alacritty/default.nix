01917b887cd09adbd76b90735c91392e0c112c87
genesis <camillemondon@free.fr>
2023-06-12 04:56:00 +0200

    profiles/alacritty: added enable option

--- profiles/home/desktop/alacritty/default.nix
+++ profiles/home/desktop/alacritty/default.nix
@@ -1,57 +1,68 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.profiles.alacritty;
+in
+with lib;
 
 {
-  programs.alacritty = {
-    enable = true;
+  options.profiles.alacritty = {
+    enable = mkEnableOption "alacritty";
+  };
 
-    settings = {
-      window = {
-        title = "Terminal";
+  config = mkIf cfg.enable {
+    programs.alacritty = {
+      enable = true;
 
-        padding = { y = 5; };
-        dimensions = {
-          lines = 75;
-          columns = 100;
+      settings = {
+        window = {
+          title = "Terminal";
+
+          padding = { y = 5; };
+          dimensions = {
+            lines = 75;
+            columns = 100;
+          };
         };
-      };
 
-      font = {
-        normal.family = "JetbrainsMono Nerd Font";
-        size = 12.0;
-      };
-
-      window.opacity = 0.8;
-
-      shell = { program = "${pkgs.fish}/bin/fish"; };
-
-      colors = {
-        primary = {
-          background = "0x000000";
-          foreground = "0xEBEBEB";
+        font = {
+          normal.family = "JetbrainsMono Nerd Font";
+          size = 12.0;
         };
-        cursor = {
-          text = "0xFF261E";
-          cursor = "0xFF261E";
-        };
-        normal = {
-          black = "0x0D0D0D";
-          red = "0xFF301B";
-          green = "0xA0E521";
-          yellow = "0xFFC620";
-          blue = "0x178AD1";
-          magenta = "0x9f7df5";
-          cyan = "0x21DEEF";
-          white = "0xEBEBEB";
-        };
-        bright = {
-          black = "0x6D7070";
-          red = "0xFF4352";
-          green = "0xB8E466";
-          yellow = "0xFFD750";
-          blue = "0x1BA6FA";
-          magenta = "0xB978EA";
-          cyan = "0x73FBF1";
-          white = "0xFEFEF8";
+
+        window.opacity = 0.8;
+
+        shell = { program = "${pkgs.fish}/bin/fish"; };
+
+        colors = {
+          primary = {
+            background = "0x000000";
+            foreground = "0xEBEBEB";
+          };
+          cursor = {
+            text = "0xFF261E";
+            cursor = "0xFF261E";
+          };
+          normal = {
+            black = "0x0D0D0D";
+            red = "0xFF301B";
+            green = "0xA0E521";
+            yellow = "0xFFC620";
+            blue = "0x178AD1";
+            magenta = "0x9f7df5";
+            cyan = "0x21DEEF";
+            white = "0xEBEBEB";
+          };
+          bright = {
+            black = "0x6D7070";
+            red = "0xFF4352";
+            green = "0xB8E466";
+            yellow = "0xFFD750";
+            blue = "0x1BA6FA";
+            magenta = "0xB978EA";
+            cyan = "0x73FBF1";
+            white = "0xFEFEF8";
+          };
         };
       };
     };
