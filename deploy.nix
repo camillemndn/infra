a110428fa0f292078eb1e701d4c2a42a55762cda
genesis <camillemondon@free.fr>
2023-06-13 00:01:27 +0200

    deploy: changed ip priority

--- deploy.nix
+++ deploy.nix
@@ -7,7 +7,7 @@
 
   nodes = lib.mapAttrs
     (machine: config: {
-      hostname = config.ipv6.public or config.ipv6.vpn or config.ipv4.vpn or config.ipv4.public;
+      hostname = config.ipv6.vpn or config.ipv4.vpn or config.ipv6.public or config.ipv4.public;
       profiles.system.path = deploy-rs.lib.${config.system or "x86_64-linux"}.activate.nixos self.nixosConfigurations.${machine};
       remoteBuild = config.remoteBuild or true;
     })
