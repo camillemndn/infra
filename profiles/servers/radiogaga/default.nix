f878d9ecc5276e512a810669545c3e64ecb8acd4
genesis <camillemondon@free.fr>
2023-06-12 23:49:37 +0200

    profiles/radiogaga: fixed after nginx changes

--- profiles/servers/radiogaga/default.nix
+++ profiles/servers/radiogaga/default.nix
@@ -6,21 +6,20 @@ in
 with lib;
 
 {
-  options.profiles.radiogaga = {
-    enable = mkEnableOption "Activate my radio alarm clock";
-  };
+  options.profiles.radiogaga.enable = mkEnableOption "Activate my radio alarm clock";
 
   config = mkIf cfg.enable {
-    networking.firewall.allowedTCPPorts = [ 80 ];
+    services = {
+      radiogaga.enable = true;
+      nginx.virtualHosts.radiogaga = {
+        listen = [{ addr = "127.0.0.1"; port = 4200; }];
+        root = "${pkgs.radiogaga}/share/radiogaga-front";
+        locations."/".tryFiles = "$uri $uri/ /index.html =404";
+        locations."/api".proxyPass = "http://127.0.0.1:8000";
+      };
 
-    services.radiogaga.enable = true;
-
-    services.nginx.virtualHosts."radiogaga.local" = {
-      default = true;
-      forceSSL = mkForce false;
-      enableACME = mkForce false;
-      root = "${pkgs.radiogaga}/share/radiogaga-front";
-      locations."/".tryFiles = "$uri $uri/ /index.html =404";
+      nginx.virtualHosts."radiogaga.lan".port = 4200;
+      nginx.virtualHosts."radiogaga.kms".port = 4200;
     };
   };
 }
