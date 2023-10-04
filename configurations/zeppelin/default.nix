4fd4a852a28868e6918cd44053e59ec9ee7c840c
genesis <camillemondon@free.fr>
2023-10-04 09:01:45 +0200

    zeppelin: added mattermost

--- configurations/zeppelin/default.nix
+++ configurations/zeppelin/default.nix
@@ -8,6 +8,8 @@ _:
 
   services = {
     hammond = { enable = true; hostName = "car.kms"; };
+    mattermost = { enable = true; siteUrl = "https://projects.mondon.xyz"; };
+    nginx.virtualHosts."projects.mondon.xyz".port = 8065;
     nginx.noDefault.enable = true;
     nginx.publicDomains = [ "mondon.xyz" "saumon.network" "yali.es" "ceciliaflamenca.com" ];
     nginx.virtualHosts."yali.es".root = "/srv/sites/yali";
