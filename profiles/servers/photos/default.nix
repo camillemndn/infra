af167cc8a639f4c68230c57bb5436326a1ced6ea
genesis <camillemondon@free.fr>
2023-10-06 00:49:15 +0200

    nixos/photos: enabled auto indexing

--- profiles/servers/photos/default.nix
+++ profiles/servers/photos/default.nix
@@ -14,6 +14,7 @@ with lib;
     services.photoprism = {
       enable = true;
       originalsPath = "/var/lib/nextcloud/data/Camille/files/Pictures";
+      importPath = "/var/lib/nextcloud/data/Camille/files/Pictures";
       passwordFile = "/run/secrets/photos";
       settings.PHOTOPRISM_ADMIN_USER = "camille";
     };
@@ -21,6 +22,18 @@ with lib;
     services.nginx.virtualHosts."photos.kms" = { port = 2342; websockets = true; };
 
     systemd.services.photoprism.serviceConfig.SupplementaryGroups = [ "nextcloud" ];
+
+    systemd.services."photoprism-index" = {
+      description = "Photoprism automatic indexer";
+      after = [ "multi-user.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "/var/lib/photoprism/photoprism-manage index --cleanup";
+        KillMode = "process";
+        Restart = "always";
+      };
+    };
 
     sops.secrets.photos = {
       format = "binary";
