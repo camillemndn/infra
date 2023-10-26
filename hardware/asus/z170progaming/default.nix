bf117a55b1291055e8a143f54e8ba89156b0d84b
Camille M. (jagger) <camillemondon@free.fr>
2023-10-26 19:39:47 +0200

    jagger: switch to systemd-stage-1

--- hardware/asus/z170progaming/default.nix
+++ hardware/asus/z170progaming/default.nix
@@ -1,9 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [ ./clevis.nix ./luksroot.nix ];
-  disabledModules = [ "system/boot/clevis.nix" "system/boot/luksroot.nix" ];
-
   boot = {
     loader = {
       systemd-boot.enable = false;
@@ -18,45 +15,22 @@
 
     initrd = {
       availableKernelModules = [ "e1000e" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-      kernelModules = [ "e1000e" ];
-
-      clevis.enable = true;
-      clevis.useTang = true;
-      clevis.devices."luks-90a7cca1-d6ee-48fb-b225-9519ad1e081b".secretFile = ./luks.jwe;
-
-      luks.devices = {
-        "luks-90a7cca1-d6ee-48fb-b225-9519ad1e081b" = {
-          device = "/dev/disk/by-uuid/90a7cca1-d6ee-48fb-b225-9519ad1e081b";
-          fallbackToPassword = true;
-          #crypttabExtraOpts = [ "_netdev" ];
-        };
-        "luks-3e4229db-fbe0-47f4-8f7c-285e2c55c268" = {
-          device = "/dev/disk/by-uuid/3e4229db-fbe0-47f4-8f7c-285e2c55c268";
-          keyFile = "/crypto_keyfile.bin";
-          #crypttabExtraOpts = [ "_netdev" ];
-        };
-      };
-
+      clevis = { enable = true; useTang = true; devices.luks-90a7cca1-d6ee-48fb-b225-9519ad1e081b.secretFile = ./luks.jwe; };
       network.enable = true;
       secrets."/crypto_keyfile.bin" = null;
-      systemd = {
-        #enable = true;
-        network = {
-          enable = true;
-          wait-online.enable = true;
+      systemd = { enable = true; network = { enable = true; wait-online.enable = true; }; };
 
-          #networks."10-static" = {
-          #  matchConfig.Name = [ "en*" "eth*" ];
-          #  DHCP = "no";
-          #  address = [ "192.168.1.4/24" ];
-          #  linkConfig.RequiredForOnline = true;
-          #  networkConfig.IPv6PrivacyExtensions = "kernel";
-          #};
+      luks.devices = {
+        luks-90a7cca1-d6ee-48fb-b225-9519ad1e081b = {
+          device = "/dev/disk/by-uuid/90a7cca1-d6ee-48fb-b225-9519ad1e081b";
+        };
+        luks-3e4229db-fbe0-47f4-8f7c-285e2c55c268 = {
+          device = "/dev/disk/by-uuid/3e4229db-fbe0-47f4-8f7c-285e2c55c268";
+          keyFile = "/crypto_keyfile.bin";
         };
       };
     };
 
-    kernelModules = [ ];
     kernelParams = [
       #"ip=dhcp"
       #"ip=enp0s31f6:dhcp"
