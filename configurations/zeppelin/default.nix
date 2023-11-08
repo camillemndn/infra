bacb55cbed174739d71838cc0176c644c1d2f89e
Camille M. (genesis) <camillemondon@free.fr>
2023-11-08 15:15:18 +0100

    zeppelin: create random-densities website

--- configurations/zeppelin/default.nix
+++ configurations/zeppelin/default.nix
@@ -15,6 +15,7 @@ _:
     nginx.publicDomains = [ "mondon.xyz" "saumon.network" "yali.es" "ceciliaflamenca.com" ];
     nginx.virtualHosts."yali.es".root = "/srv/sites/yali";
     nginx.virtualHosts."ceciliaflamenca.com".root = "/srv/sites/cecilia-flamenca";
+    nginx.virtualHosts."camille.mondon.xyz".locations."/random-densities".root = "/srv/sites/random-densities";
     openssh.enable = true;
     tailscale.enable = true;
     webtrees = { enable = true; hostName = "family.mondon.xyz"; };
