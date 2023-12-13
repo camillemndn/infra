617ea3266266a82c30163894d878b03ee8410d91
Camille M. (thelonious) <camillemondon@free.fr>
2023-12-13 19:58:49 +0100

    hardware/dell/precision3240: init

---
+++ hardware/dell/precision3240/default.nix
@@ -0,0 +1,47 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot = {
+    loader = {
+      systemd-boot.enable = lib.mkForce false;
+      efi.canTouchEfiVariables = true;
+    };
+
+    initrd = {
+      clevis = { enable = true; devices.luks-0a53c761-f5d4-4015-9d68-349251b71c85.secretFile = ./luks.jwe; };
+      luks.devices."luks-c64c8ddc-e96f-453d-b445-98bef8fd0803".device = "/dev/disk/by-uuid/c64c8ddc-e96f-453d-b445-98bef8fd0803";
+      luks.devices."luks-0a53c761-f5d4-4015-9d68-349251b71c85".device = "/dev/disk/by-uuid/0a53c761-f5d4-4015-9d68-349251b71c85";
+      availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "tpm_tis" ];
+      kernelModules = [ ];
+    };
+
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+    kernelParams = [ "tpm_tis.interrupts=0" ];
+
+    lanzaboote = {
+      enable = true;
+      publicKeyFile = "/etc/secureboot/keys/db/db.pem"; # DB public key
+      privateKeyFile = "/etc/secureboot/keys/db/db.key"; # DB private key
+      pkiBundle = "/etc/secureboot/";
+      enrollKeys = true;
+      configurationLimit = 5;
+    };
+  };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/959a07ef-a27d-4186-87da-9ed23dc4aa33";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/6851-8BFB";
+    fsType = "vfat";
+  };
+
+  swapDevices = [{ device = "/dev/disk/by-uuid/2deec870-eb12-4930-843f-c6603e27a424"; }];
+
+  networking.useDHCP = lib.mkDefault true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
