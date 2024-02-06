742235229a9f9ab75e16ac9ac11e5a6bdd016e7d
Camille M. (thelonious) <camillemondon@free.fr>
2024-02-06 19:56:14 +0100

    Revert "nixos/browser: force install firefoxpwa addon"
    This reverts commit 02b3fa2bee77c233453d75fed57fc27396326fd2.

--- profiles/desktop/browser/default.nix
+++ profiles/desktop/browser/default.nix
@@ -19,12 +19,6 @@ with lib;
         "browser.fixup.domainsuffixwhitelist.saumon" = true;
       };
       preferencesStatus = "default";
-      policies = {
-        ExtensionSettings."firefoxpwa@filips.si" = {
-          installation_mode = "force_installed";
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/pwas-for-firefox";
-        };
-      };
       wrapperConfig.firefox = {
         alsaSupport = true;
         ffmpegSupport = true;
