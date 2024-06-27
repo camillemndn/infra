aace2325978f261f254860772a0e8e16a9b55e16
Camille M. (genesis) <camillemondon@free.fr>
2024-06-27 10:03:39 +0200

    don't use patches for now

--- patches/default.nix
+++ patches/default.nix
@@ -1,6 +1,1 @@
-{
-  tzupdate = builtins.fetchurl {
-    url = "https://github.com/NixOS/nixpkgs/pull/316133/commits/3199ba030e1071470a7651a780041b0169e9d0b9.patch";
-    sha256 = "0qqa8da0miba4v0i8ag4021n77dcw1hspc4wm6ic9a6bwvzvdmr4";
-  };
-}
+{ }
