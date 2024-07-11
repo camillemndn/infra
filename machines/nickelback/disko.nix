fb00eae30fadb676e5b53cec5125e3b6cdc6f582
Camille M. (genesis) <camillemondon@free.fr>
2024-07-11 03:08:12 +0200

    feat: nickelback: create disko config

---
+++ machines/nickelback/disko.nix
@@ -0,0 +1,44 @@
+{
+  disko.devices.disk.main = {
+    device = "/dev/disk/by-id/nvme-CT2000P2SSD8_2151E5F38722";
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          size = "500M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+          };
+        };
+        root = {
+          end = "-1G";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+        encryptedSwap = {
+          size = "10M";
+          content = {
+            type = "swap";
+            randomEncryption = true;
+            priority = 100; # prefer to encrypt as long as we have space for it
+          };
+        };
+        plainSwap = {
+          size = "100%";
+          content = {
+            type = "swap";
+            discardPolicy = "both";
+            resumeDevice = true; # resume from hiberation from this device
+          };
+        };
+      };
+    };
+  };
+}
--- machines/nickelback/hardware-configuration.nix
+++ machines/nickelback/hardware-configuration.nix
@@ -6,6 +6,8 @@
 }:
 
 {
+  imports = [ ./disko.nix ];
+
   boot = {
     lanzaboote = {
       enable = true;
@@ -28,16 +30,4 @@
   environment.systemPackages = [ pkgs.sbctl ];
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/dc9ab470-b542-43c3-bf54-a438d71b33a5";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/9DEF-35FE";
-    fsType = "vfat";
-  };
-
-  swapDevices = [ { device = "/dev/disk/by-uuid/fadaee1c-9820-4c06-a144-f3d293e0a4e3"; } ];
 }
