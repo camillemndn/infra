a9d0fefd22f023a93f531614bea6916920adb6ac
Camille M. (genesis) <camillemondon@free.fr>
2024-07-26 21:46:18 +0200

    fix: webhook user to camille

--- profiles/webhook/default.nix
+++ profiles/webhook/default.nix
@@ -6,53 +6,57 @@
 }:
 
 lib.mkIf config.services.webhook.enable {
-  services.webhook.hooks = {
-    redeploy-website = {
-      command-working-directory = "/srv/sites";
-      execute-command = "${pkgs.writeShellScript "redeploy-website" ''
-        if [ ! -d "$1-git" ]; then
-          ${pkgs.git}/bin/git clone "git@github.com:camillemndn/$1.git"
-        fi
+  services.webhook = {
+    user = "camille";
+    group = "users";
+    hooks = {
+      redeploy-website = {
+        command-working-directory = "/srv/sites";
+        execute-command = "${pkgs.writeShellScript "redeploy-website" ''
+          if [ ! -d "$1-git" ]; then
+            ${pkgs.git}/bin/git clone "git@github.com:camillemndn/$1.git"
+          fi
 
-        pushd "$1-git"
-        ${pkgs.git}/bin/git pull
-        ${pkgs.nix}/bin/nix-build -A packages.x86_64-linux.website
-        popd
+          pushd "$1-git"
+          ${pkgs.git}/bin/git pull
+          ${pkgs.nix}/bin/nix-build -A packages.x86_64-linux.website
+          popd
 
-        if [ ! -d "$1" ]; then
-          rm -r "$1"
-        fi
-        cp -r "$1-git/result" "$1"
-      ''}";
-      pass-arguments-to-command = [
-        {
-          source = "payload";
-          name = "repository.name";
-        }
-      ];
-      trigger-rule = {
-        and = [
+          if [ ! -d "$1" ]; then
+            rm -r "$1"
+          fi
+          cp -r "$1-git/result" "$1"
+        ''}";
+        pass-arguments-to-command = [
           {
-            match = {
-              type = "payload-hash-sha1";
-              secret = "verysafesecret";
-              parameter = {
-                source = "header";
-                name = "X-Hub-Signature";
-              };
-            };
-          }
-          {
-            match = {
-              type = "value";
-              value = "tag";
-              parameter = {
-                "source" = "payload";
-                "name" = "ref_type";
-              };
-            };
+            source = "payload";
+            name = "repository.name";
           }
         ];
+        trigger-rule = {
+          and = [
+            {
+              match = {
+                type = "payload-hash-sha1";
+                secret = "verysafesecret";
+                parameter = {
+                  source = "header";
+                  name = "X-Hub-Signature";
+                };
+              };
+            }
+            {
+              match = {
+                type = "value";
+                value = "tag";
+                parameter = {
+                  "source" = "payload";
+                  "name" = "ref_type";
+                };
+              };
+            }
+          ];
+        };
       };
     };
   };
