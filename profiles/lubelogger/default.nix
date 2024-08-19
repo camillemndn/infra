bec0af013a3abfb9805a0c712064de4a4092dd2d
Camille M. (genesis) <camillemondon@free.fr>
2024-08-19 18:27:31 +0200

    fix: lubelogger: add nginx config

--- profiles/lubelogger/default.nix
+++ profiles/lubelogger/default.nix
@@ -12,6 +12,17 @@ lib.mkIf config.services.lubelogger.enable {
     nginx.virtualHosts."vehicles.kms" = {
       port = 5003;
       basicAuthFile = "/var/lib/lubelogger_auth";
+      locations."/".extraConfig = ''
+        client_max_body_size               50000M;
+        proxy_set_header Host              $http_host;
+        proxy_set_header X-Real-IP         $remote_addr;
+        proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_http_version 1.1;
+        proxy_set_header   Upgrade    $http_upgrade;
+        proxy_set_header   Connection "upgrade";
+        proxy_redirect off;
+      '';
     };
   };
 }
