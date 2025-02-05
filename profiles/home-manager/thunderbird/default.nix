dd81c5214f19140ea83001ba440cfb6cd52e2a97
Camille M. (genesis) <camillemondon@free.fr>
2025-02-05 01:17:30 +0100

    feat: thunderbird: varanda

--- profiles/home-manager/thunderbird/default.nix
+++ profiles/home-manager/thunderbird/default.nix
@@ -1,74 +1,91 @@
 { config, lib, ... }:
 
-lib.mkIf config.programs.thunderbird.enable {
-  programs.thunderbird = {
-    profiles."camille" = {
+with lib;
+
+{
+  options.accounts.email.accounts = mkOption {
+    type = types.attrsOf (
+      types.submodule (
+        { name, ... }:
+        {
+          config = mkDefault {
+            address = name;
+            realName = "Camille Mondon";
+            thunderbird = {
+              enable = true;
+              profiles = [ "camille" ];
+            };
+          };
+        }
+      )
+    );
+  };
+
+  config = lib.mkIf config.programs.thunderbird.enable {
+    programs.thunderbird.profiles."camille" = {
       isDefault = true;
       settings = {
         "intl.locale.requested" = "fr,en-US";
         "mail.identity.id3.organization" = "TSE-R";
-        "mail.identity.id3.htmlSigText" = ''
-          <small><p><b>Camille MONDON<br></b>
-          Doctorant<br>
-          <i>TSE-R | Bureau T.234<br>
-          1, Esplanade de l'Université 31000 Toulouse</i></p></small>
-        '';
         "mail.identity.id3.sig_on_fwd" = true;
       };
     };
-  };
 
-  accounts.email = {
-    accounts."camillemondon@online.fr" = {
-      realName = "Camille Mondon";
-      address = "camillemondon@online.fr";
-      userName = "camillemondon";
-      primary = true;
-      imap = {
-        host = "imap.free.fr";
-        port = 993;
+    accounts.email.accounts = {
+      "camillemondon@online.fr" = {
+        userName = "camillemondon";
+        primary = true;
+        imap = {
+          host = "imap.free.fr";
+          port = 993;
+        };
+        smtp = {
+          host = "smtp.free.fr";
+          port = 465;
+        };
       };
-      smtp = {
-        host = "smtp.free.fr";
-        port = 465;
+      "camille.mondon@ens.fr" = {
+        userName = "cmondon02";
+        imap = {
+          host = "clipper.ens.fr";
+          port = 993;
+        };
+        smtp = {
+          host = "clipper.ens.fr";
+          port = 465;
+        };
       };
-      thunderbird = {
-        enable = true;
-        profiles = [ "camille" ];
+      "camille.mondon@tse-fr.eu" = {
+        userName = "camille.mondon@tse-fr.eu";
+        imap = {
+          host = "mail.saumon.network";
+          port = 1143;
+        };
+        smtp = {
+          host = "mail.saumon.network";
+          port = 1025;
+        };
+        signature = {
+          showSignature = "append";
+          text = ''
+            <small><p><b>Camille MONDON<br></b>
+            Doctorant<br>
+            <i>TSE-R | Bureau T.234<br>
+            1, Esplanade de l'Université 31000 Toulouse</i></p></small>
+          '';
+        };
       };
-    };
-    accounts."camille.mondon@ens.fr" = {
-      realName = "Camille Mondon";
-      address = "camille.mondon@ens.fr";
-      userName = "cmondon02";
-      imap = {
-        host = "clipper.ens.fr";
-        port = 993;
-      };
-      smtp = {
-        host = "clipper.ens.fr";
-        port = 465;
-      };
-      thunderbird = {
-        enable = true;
-        profiles = [ "camille" ];
-      };
-    };
-    accounts."camille.mondon@tse-fr.eu" = {
-      realName = "Camille Mondon";
-      address = "camille.mondon@tse-fr.eu";
-      userName = "camille.mondon@tse-fr.eu";
-      imap = {
-        host = "mail.saumon.network";
-        port = 1143;
-      };
-      smtp = {
-        host = "mail.saumon.network";
-        port = 1025;
-      };
-      thunderbird = {
-        enable = true;
-        profiles = [ "camille" ];
+      "contact@varanda.fr" = {
+        realName = "Varanda";
+        userName = "varanda";
+        imap = {
+          host = "mail.luj.fr";
+          port = 993;
+        };
+        smtp = {
+          host = "mail.luj.fr";
+          port = 465;
+        };
       };
     };
   };
