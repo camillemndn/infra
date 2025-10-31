7fd4210b7c899f17cc62e2c3e69d9113dda7215a
Camille M. (genesis) <camillemondon@free.fr>
2025-10-31 11:26:54 +0100

    feat: nixvim: bufferline and cmp

--- profiles/nixvim/default.nix
+++ profiles/nixvim/default.nix
@@ -23,14 +23,27 @@
     };
 
     plugins = {
+      bufferline.enable = true;
+
       cmp = {
         enable = true;
         autoEnableSources = true;
-        settings.sources = [
-          { name = "nvim_lsp"; }
-          { name = "path"; }
-          { name = "buffer"; }
-        ];
+        settings = {
+          mapping = {
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-e>" = "cmp.mapping.close()";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          };
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          ];
+        };
       };
 
       colorizer.enable = true;
@@ -78,7 +91,11 @@
         };
       };
 
-      vim-slime.enable = true;
+      vim-slime = {
+        enable = true;
+        settings.target = "neovim";
+      };
+
       web-devicons.enable = true;
     };
 
