f70cc5ef5678e058609b1b8271f96f6423195f11
Camille M. (genesis) <camillemondon@free.fr>
2025-11-28 13:49:25 +0100

    feat: offspring: wireguard

--- machines/offspring/default.nix
+++ machines/offspring/default.nix
@@ -5,7 +5,26 @@ _:
 
   networking = {
     hostName = "offspring";
-    firewall.allowedTCPPorts = [ 2022 ];
+
+    firewall = {
+      allowedTCPPorts = [ 2022 ];
+      allowedUDPPorts = [ 51820 ];
+    };
+
+    wireguard.interfaces.wg0 = {
+      ips = [ "10.100.46.1/24" ];
+      allowedIPsAsRoutes = false;
+      privateKeyFile = "/etc/wireguard/privatekey";
+      listenPort = 51820;
+
+      peers = [
+        {
+          # quilapayun
+          publicKey = "sF4B+6Lw6wNqIb+uV3STbS5nX8x+jO1Udo9U471ciFY=";
+          allowedIPs = [ "10.100.46.2/32" ];
+        }
+      ];
+    };
   };
 
   deployment.targetHost = "offspring.mondon.xyz";
@@ -16,8 +35,17 @@ _:
 
   services = {
     openssh.enable = true;
-    nginx.enable = true;
-    nginx.noDefault.enable = true;
+
+    nginx = {
+      enable = true;
+      noDefault.enable = true;
+      publicDomains = [
+        "mondon.xyz"
+        "saumon.network"
+      ];
+      virtualHosts."lists.saumon.network".locations."/".proxyPass = "http://10.100.46.2";
+    };
+
     uptime-kuma.enable = true;
   };
 
