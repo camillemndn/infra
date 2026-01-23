d96b2e102bf5337526882c0846b31892a53fa4d7
Camille M. (genesis) <camillemondon@free.fr>
2026-01-23 18:49:02 +0100

    feat: plasma: kclock

--- profiles/plasma/default.nix
+++ profiles/plasma/default.nix
@@ -6,7 +6,10 @@
 }:
 
 lib.mkIf config.services.desktopManager.plasma6.enable {
-  environment.plasma6.excludePackages = [ pkgs.kdePackages.konsole ];
+  environment = {
+    plasma6.excludePackages = [ pkgs.kdePackages.konsole ];
+    systemPackages = with pkgs.kdePackages; [ kclock ];
+  };
 
   programs.partition-manager.enable = true;
 }
