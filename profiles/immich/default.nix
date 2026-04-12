60ada805571c9bbd4e958d706565030c693ba3b2
Camille M. (genesis) <camillemondon@free.fr>
2026-04-13 01:42:20 +0200

    fix(immich): upload from mobile works

--- profiles/immich/default.nix
+++ profiles/immich/default.nix
@@ -8,12 +8,19 @@
 lib.mkIf config.services.immich.enable {
   services = {
     immich = {
-      database.enable = true;
+      database = {
+        enable = true;
+        enableVectors = false;
+      };
+      host = "127.0.0.1";
     };
 
     nginx.virtualHosts."gallery.kms" = {
       inherit (config.services.immich) port;
       websockets = true;
+      extraConfig = ''
+        client_max_body_size 0;
+      '';
     };
   };
 }
